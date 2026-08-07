bd0e1281d9553de09f75a6246cadc0bc9319d967
woorld <17719812+woorld@users.noreply.github.com>
2026-08-07 21:47:20 +0900

    役一覧のカードコンポーネントを切り出し、役情報に合わせて表示するよう修正

---
+++ src/components/PointCard.tsx
@@ -0,0 +1,61 @@
+import 'bulma/css/bulma.css'
+import {useState} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAngleDown} from "@fortawesome/free-solid-svg-icons"
+import {faAngleUp} from "@fortawesome/free-solid-svg-icons"
+import type { FranChallengeHand } from "../constants"
+
+type Props = FranChallengeHand & { defaultCollapsed?: boolean }
+
+export default function PointCard({
+    name,
+    description,
+    points,
+    probability,
+    canComposite,
+    isPokerHand,
+    defaultCollapsed = true,
+}: Props) {
+    const [collapsed, setCollapsed] = useState(defaultCollapsed)
+    const handType = `${canComposite ? '複合' : '単独'}役${isPokerHand ? '・ポーカー役' : ''}`
+
+    if (isPokerHand) {
+        description += '（他のポーカー役とは複合しない）'
+    }
+
+    return (
+        <>
+            <div style={{marginTop: '2rem', width: '80vw', maxWidth: '600px'}}>
+                <div className="card">
+                    <header className="card-header">
+                        <p className="card-header-title">{name}</p>
+                        <button
+                            className="card-header-icon"
+                            aria-label="toggle content"
+                            aria-expanded={!collapsed}
+                            onClick={() => setCollapsed(prev => !prev)}
+                        >
+                            <span className="icon">
+                                <FontAwesomeIcon icon={collapsed ? faAngleDown : faAngleUp}/>
+                            </span>
+                        </button>
+                    </header>
+                    {!collapsed && description && (
+                        <div className="card-content">
+                            <p style={{marginBottom: '1rem'}}>{handType}</p>
+                            <div className="content">{description}</div>
+                        </div>
+                    )}
+                    {!collapsed && points !== undefined && (
+                        <footer className="card-footer">
+                            <p className="card-footer-item">
+                                {points}点
+                                {probability && (<>（1/{probability}）</>)}
+                            </p>
+                        </footer>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
